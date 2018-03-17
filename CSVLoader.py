ad761081421b42fe93b2ef19afe73a3880dae1ed
Michael Austin <maxaustin92@gmail.com>
2018-03-17 16:34:27 -0400

    Initial commit

---
+++ CSVLoader.py
@@ -0,0 +1,12 @@
+import csv
+import os
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('hrtdz_data.csv')
+
+print(df.info())
+print("---------------------------------------")
+print()
+print(df)
+
